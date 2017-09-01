2522f6f928b682523e0a8ced9b63be49f377da46
Shamoun Syed <shamoun723@gmail.com>
2017-09-01 13:12:30 -0700

    feat: implement instant search results

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -15,17 +15,38 @@ class SearchBooks extends Component {
     onBookSearch: PropTypes.func.isRequired
   }
 
+  constructor(props) {
+    super(props)
+    this.searchTerms = ['Android', 'Art', 'Artificial Intelligence',
+      'Astronomy', 'Austen', 'Baseball', 'Basketball', 'Bhagat',
+      'Biography', 'Brief', 'Business', 'Camus', 'Cervantes',
+      'Christie', 'Classics', 'Comics', 'Cook', 'Cricket',
+      'Cycling', 'Desai', 'Design', 'Development',
+      'Digital Marketing', 'Drama', 'Drawing', 'Dumas',
+      'Education', 'Everything', 'Fantasy', 'Film', 'Finance',
+      'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi',
+      'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka',
+      'King', 'Lahiri', 'Larsson', 'Learn', 'Literary Fiction',
+      'Make', 'Manage', 'Marquez', 'Money', 'Mystery', 'Negotiate',
+      'Painting', 'Philosophy', 'Photography', 'Poetry', 'Production',
+      'Programming', 'React', 'Redux', 'River', 'Robotics', 'Rowling',
+      'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming',
+      'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate',
+      'Virtual Reality', 'Web Development', 'iOS']
+  }
+
   state = {
     query: ''
   }
 
   updateQuery = (query) => {
-    this.setState({ query: query.trim()})
-  }
+    this.setState({ query: query })
 
-  handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.onBookSearch(this.state.query)
+    const match = new RegExp("^" + query + "$", 'i')
+    let result = this.searchTerms.filter((term) => match.test(term))
+    if (result.length === 1) {
+      this.props.onBookSearch(result.toString())
+    }
   }
 
   getShelf = (id) => {
@@ -43,12 +64,10 @@ class SearchBooks extends Component {
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            <form onSubmit={this.handleSubmit}>
-              <input type="text"
-                     placeholder="Search by title or author"
-                     value={this.state.query}
-                     onChange={(event) => (this.updateQuery(event.target.value))}/>
-            </form>
+            <input type="text"
+                   placeholder="Search by title or author"
+                   value={this.state.query}
+                   onChange={(event) => (this.updateQuery(event.target.value))}/>
           </div>
         </div>
         <div className="search-books-results">
